10288c25dca9f8b233b44e322aec2532972d1573
B077AS <118758182+B077AS@users.noreply.github.com>
2023-01-02 22:11:35 +0100

    auto delete old notifications

--- TempProjectTesting/src/Users/Professors/Teachers.java
+++ TempProjectTesting/src/Users/Professors/Teachers.java
@@ -2,9 +2,14 @@ package Users.Professors;
 
 import java.awt.Image;
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.swing.*;
 import DataBase.DBConnection;
 import Login.*;
+import MyTimer.MyTimer;
 import Users.GeneralUser.Users;
 import Users.GeneralUser.UsersGUI;
 
@@ -19,7 +24,7 @@ public class Teachers extends Users{
 	public void GUI(LoginGUI frame) {
 		TeachersGUI gui=new TeachersGUI(this.name, this.lastName, this.email, this);
 		frame.dispose();
-		
+
 	}
 
 	@Override
@@ -27,7 +32,7 @@ public class Teachers extends Users{
 		boolean profRequests=false;
 		boolean swapRequests=false;
 		boolean reminders=false;;
-		
+		deleteOldNotifications();
 		try {
 
 			Connection conn=DBConnection.connect();
@@ -35,25 +40,25 @@ public class Teachers extends Users{
 			PreparedStatement statement = conn.prepareStatement(query);
 			statement.setString(1, this.ID);
 			ResultSet result=statement.executeQuery();
-			
+
 			profRequests=result.next();
-			
+
 			query="select * from swap_notifications where Receiver=? and Accepted='false'";
 			statement = conn.prepareStatement(query);
 			statement.setString(1, this.ID);
 			result=statement.executeQuery();
-			
+
 			swapRequests=result.next();
-			
+
 			query="select * from swap_notifications where Receiver=? or Sender=? and Accepted='true'";
 			statement = conn.prepareStatement(query);
 			statement.setString(1, this.ID);
 			statement.setString(2, this.ID);
 			result=statement.executeQuery();
-			
+
 			reminders=result.next();
-			
-			
+
+
 			if (profRequests==true || swapRequests==true) {
 				conn.close();
 				ImageIcon notificationIcon=new ImageIcon("Files/bell-icon-active.png");
@@ -77,7 +82,7 @@ public class Teachers extends Users{
 				Image newimg = image.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);  
 				notificationIcon = new ImageIcon(newimg);
 				return new JButton(notificationIcon);
-				
+
 			}
 
 
@@ -103,5 +108,69 @@ public class Teachers extends Users{
 	@Override
 	public JPanel notificationPanel(Users user, UsersGUI frame) {
 		return new TeachersNotificationPanel(user, frame);
+	}
+
+	public void deleteOldNotifications() {
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate now=LocalDate.now();
+		dtf.format(now);
+		
+		try {
+
+			Connection conn=DBConnection.connect();
+			String query="select * from prof_notifications";
+			Statement statement = conn.createStatement();
+			ResultSet result=statement.executeQuery(query);
+			
+			while(result.next()) {
+				if(result.getDate(4)!=null && now.compareTo((result.getDate(4)).toLocalDate())>0) {
+					query="delete from prof_notifications where Schedule_ID=? and Date=? and Sender=? and New_Date=? and New_From=? and New_To=?";
+					PreparedStatement preparedStmt = conn.prepareStatement(query);
+					preparedStmt.setString(1, result.getString(1));
+					preparedStmt.setDate(2, result.getDate(2));
+					preparedStmt.setString(3, result.getString(3));
+					preparedStmt.setDate(4, result.getDate(4));
+					preparedStmt.setString(5, result.getString(5));
+					preparedStmt.setString(6, result.getString(6));
+					preparedStmt.executeUpdate();
+
+					}
+					else if (result.getDate(4)==null && now.compareTo((result.getDate(2)).toLocalDate())>0){
+						query="delete from prof_notifications where Schedule_ID=? and Date=? and Sender=? and New_Date is NULL";
+						PreparedStatement preparedStmt = conn.prepareStatement(query);
+						preparedStmt.setString(1, result.getString(1));
+						preparedStmt.setDate(2, result.getDate(2));
+						preparedStmt.setString(3, result.getString(3));
+						preparedStmt.executeUpdate();
+						conn.close();
+					}
+				}
+			
+			query="select * from swap_notifications";
+			statement = conn.createStatement();
+			result=statement.executeQuery(query);
+			
+
+			while(result.next()) {
+				if(now.compareTo((result.getDate(4)).toLocalDate())>0) {
+					query="delete from swap_notifications where Sender=? and Receiver=? and First_Date=? and New_Date=? and First_Schedule=? and New_Schedule=? and Accepted=?";
+					PreparedStatement preparedStmt = conn.prepareStatement(query);
+					preparedStmt.setString(1, result.getString(1));
+					preparedStmt.setString(2, result.getString(2));
+					preparedStmt.setDate(3, result.getDate(3));
+					preparedStmt.setDate(4, result.getDate(4));
+					preparedStmt.setString(5, result.getString(5));
+					preparedStmt.setString(6, result.getString(6));
+					preparedStmt.setString(7, result.getString(7));
+					preparedStmt.executeUpdate();
+					}
+				}
+			
+			conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
